3c19fdc9c8ce9b8c77d030f2483458de6cc5f3a6
parkermax93 <parker.williamson@gmail.com>
2020-09-23 14:54:47 -0700

    remove userid from comments

--- db/migrate/20200922180736_create_comment.rb
+++ db/migrate/20200922180736_create_comment.rb
@@ -2,7 +2,7 @@ class CreateComment < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
       t.text :content
-      t.integer :user_id
+      t.text :name
       t.integer :character_id
       t.timestamps
     end
